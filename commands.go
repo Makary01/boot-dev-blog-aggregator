19b60fcd91220e5ae89da1da83e7eb027e419eff
Makary01 <makary.bortnowski@gmail.com>
2026-07-26 11:12:30 +0200

    move commands to different file

--- commands.go
+++ commands.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"Makary01/boot-dev-blog-aggregator/internal/database"
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+)
 
 type command struct {
 	name string
@@ -21,4 +27,63 @@ func (c *commands) run(s *state, cmd command) error {
 
 func (c *commands) register(name string, handler func(*state, command) error) {
 	c.handlersByName[name] = handler
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if err := checkArgsLen(cmd.args, 1); err != nil {
+		return err
+	}
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println("User has been set!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if err := checkArgsLen(cmd.args, 1); err != nil {
+		return err
+	}
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User created: %v\n", user)
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if err := checkArgsLen(cmd.args, 0); err != nil {
+		return err
+	}
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkArgsLen(args []string, expected int) error {
+	if len(args) != expected {
+		return fmt.Errorf("expected '%v' argument, got '%v'\n", expected, len(args))
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"Makary01/boot-dev-blog-aggregator/internal/config"
 	"Makary01/boot-dev-blog-aggregator/internal/database"
-	"context"
 	"database/sql"
 	"fmt"
-	"os"
-	"time"
-
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 type state struct {
@@ -49,63 +45,4 @@ func main() {
 		fmt.Printf("Error running command: %v\n", err.Error())
 		os.Exit(1)
 	}
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if err := checkArgsLen(cmd.args, 1); err != nil {
-		return err
-	}
-	_, err := s.db.GetUser(context.Background(), cmd.args[0])
-	if err != nil {
-		return err
-	}
-
-	err = s.config.SetUser(cmd.args[0])
-	if err != nil {
-		return err
-	}
-	fmt.Println("User has been set!")
-	return nil
-}
-
-func handlerRegister(s *state, cmd command) error {
-	if err := checkArgsLen(cmd.args, 1); err != nil {
-		return err
-	}
-	params := database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-		Name:      cmd.args[0],
-	}
-	user, err := s.db.CreateUser(context.Background(), params)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("User created: %v\n", user)
-
-	err = s.config.SetUser(user.Name)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func handlerReset(s *state, cmd command) error {
-	if err := checkArgsLen(cmd.args, 0); err != nil {
-		return err
-	}
-	err := s.db.DeleteUsers(context.Background())
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func checkArgsLen(args []string, expected int) error {
-	if len(args) != expected {
-		return fmt.Errorf("expected '%v' argument, got '%v'\n", expected, len(args))
-	}
-	return nil
 }
